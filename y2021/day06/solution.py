c56af9e424cbb5853924b77511d62e082a60a1d9
Matt Kramer <mkramer@anaconda.com>
2021-12-06 08:33:43 -0600

    Refactor and solve part 2

--- y2021/day06/solution.py
+++ y2021/day06/solution.py
@@ -1,4 +1,6 @@
+from collections import defaultdict
 from pathlib import Path
+from typing import Dict
 from typing import List
 
 
@@ -9,40 +11,34 @@ def parse(input_str: str) -> List[int]:
     return [int(s) for s in input_str.split(",")]
 
 
-def advance(data: List[int]) -> List[int]:
-    new_data = []
-    num_new = 0
-    for fish in data:
-        if fish == 0:
-            new_data.append(6)
-            num_new += 1
+def advance(data: Dict[int, int]) -> Dict[int, int]:
+    new_data: Dict[int, int] = defaultdict(lambda: 0)
+    for age, count in data.items():
+        if age == 0:
+            new_data[6] += count
+            new_data[8] += count
         else:
-            new_data.append(fish - 1)
-
-    for _ in range(num_new):
-        new_data.append(8)
+            new_data[age - 1] += count
     return new_data
 
 
-def calculate_part1(input_str: str) -> int:
+def calculate(input_str: str, generations: int) -> int:
     data = parse(input_str)  # noqa: F841
-    for i in range(80):
-        data = advance(data)
-    return len(data)
 
+    fish_counts: Dict[int, int] = defaultdict(lambda: 0)
+    for fish in data:
+        fish_counts[fish] += 1
 
-def calculate_part2(input_str: str) -> int:
-    data = parse(input_str)  # noqa: F841
-    for i in range(256):
-        data = advance(data)
-    return len(data)
+    for i in range(generations):
+        fish_counts = advance(fish_counts)
+    return sum(fish_counts.values())
 
 
 def main() -> None:
     with INPUTS_FILE.open() as fp:
         input_str = fp.read()
-        print(f"The answer to part 1 is {calculate_part1(input_str)}")
-        print(f"The answer to part 2 is {calculate_part2(input_str)}")
+        print(f"The answer to part 1 is {calculate(input_str, generations=80)}")
+        print(f"The answer to part 2 is {calculate(input_str, generations=256)}")
 
 
 if __name__ == "__main__":
--- y2021/day06/tests.py
+++ y2021/day06/tests.py
@@ -2,8 +2,7 @@ from pathlib import Path
 
 import pytest
 
-from .solution import calculate_part1
-from .solution import calculate_part2
+from .solution import calculate
 
 TEST_INPUT = "3,4,3,1,2"
 
@@ -20,15 +19,15 @@ with INPUTS_FILE.open("r") as fp:
     ],
 )
 def test_part1(input_str: str, expected: int) -> None:
-    assert calculate_part1(input_str) == expected
+    assert calculate(input_str, generations=80) == expected
 
 
 @pytest.mark.parametrize(
     "input_str,expected",
     [
         (TEST_INPUT, 26984457539),
-        # (REAL_INPUT, 50),
+        (REAL_INPUT, 1622533344325),
     ],
 )
 def test_part2(input_str: str, expected: int) -> None:
-    assert calculate_part2(input_str) == expected
+    assert calculate(input_str, generations=256) == expected
