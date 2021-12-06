c62c2603ad921d772349e54cd983112a87b63fc4
Matt Kramer <mkramer@anaconda.com>
2021-12-05 23:00:37 -0600

    Update template

--- template/tests.py
+++ template/tests.py
@@ -1,19 +1,26 @@
+from pathlib import Path
+
 import pytest
 
 from .solution import calculate_part1
 from .solution import calculate_part2
 
+TEST_INPUT = """
+    some_input
+    abc
+"""
+
+
+INPUTS_FILE = Path(__file__).parent / "input.txt"
+with INPUTS_FILE.open("r") as fp:
+    REAL_INPUT = fp.read()
+
 
 @pytest.mark.parametrize(
     "input_str,expected",
     [
-        (
-            """
-            some_input
-            abc
-            """,
-            10,
-        )
+        (TEST_INPUT, 10),
+        # (REAL_INPUT, 50),
     ],
 )
 def test_part1(input_str: str, expected: int) -> None:
@@ -23,13 +30,8 @@ def test_part1(input_str: str, expected: int) -> None:
 @pytest.mark.parametrize(
     "input_str,expected",
     [
-        (
-            """
-            some_input
-            abc
-            """,
-            15,
-        )
+        (TEST_INPUT, 10),
+        # (REAL_INPUT, 50),
     ],
 )
 def test_part2(input_str: str, expected: int) -> None:
