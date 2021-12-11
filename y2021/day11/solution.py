9e3f9240eae719aaac3eddf3c6641d6c86abce49
Matt Kramer <mkramer@anaconda.com>
2021-12-10 23:49:56 -0600

    Solve day 11 part 1

---
+++ y2021/day11/solution.py
@@ -0,0 +1,56 @@
+import itertools
+from pathlib import Path
+from typing import Dict
+from typing import Tuple
+
+INPUTS_FILE = Path(__file__).parent / "input.txt"
+
+
+def parse(input_str: str) -> Dict[Tuple[int, int], int]:
+    data = {}
+    for i, line in enumerate(input_str.strip().split()):
+        for j, char in enumerate(line.strip()):
+            data[i, j] = int(char)
+
+    return data
+
+
+def calculate_part1(input_str: str, num_steps: int) -> int:
+    data = parse(input_str)  # noqa: F841
+    num_blinked = 0
+    for step in range(num_steps):
+        for pos in data:
+            data[pos] += 1
+
+        blinked = set()
+        while any(i > 9 for i in data.values()):
+            for pos in dict(data):
+                if data[pos] > 9 and pos not in blinked:
+                    blinked.add(pos)
+                    data.pop(pos)
+                    for dx, dy in itertools.product([-1, 0, 1], [-1, 0, 1]):
+                        new_pos = (pos[0] + dx, pos[1] + dy)
+                        if new_pos in data:
+                            data[new_pos] += 1
+
+        num_blinked += len(blinked)
+        for pos in blinked:
+            data[pos] = 0
+
+    return num_blinked
+
+
+def calculate_part2(input_str: str) -> int:
+    data = parse(input_str)  # noqa: F841
+    raise ValueError("Cannot find an answer")
+
+
+def main() -> None:
+    with INPUTS_FILE.open() as fp:
+        input_str = fp.read()
+        print(f"The answer to part 1 is {calculate_part1(input_str, num_steps=100)}")
+        print(f"The answer to part 2 is {calculate_part2(input_str)}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ y2021/day11/tests.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+
+import pytest
+
+from .solution import calculate_part1
+from .solution import calculate_part2
+
+TEST_INPUT = """
+    5483143223
+    2745854711
+    5264556173
+    6141336146
+    6357385478
+    4167524645
+    2176841721
+    6882881134
+    4846848554
+    5283751526
+"""
+
+
+INPUTS_FILE = Path(__file__).parent / "input.txt"
+with INPUTS_FILE.open("r") as fp:
+    REAL_INPUT = fp.read()
+
+
+@pytest.mark.parametrize(
+    "input_str,expected,num_steps",
+    [
+        (TEST_INPUT, 204, 10),
+        (TEST_INPUT, 1656, 100),
+        (REAL_INPUT, 1743, 100),
+    ],
+)
+def test_part1(input_str: str, expected: int, num_steps: int) -> None:
+    assert calculate_part1(input_str, num_steps) == expected
+
+
+@pytest.mark.parametrize(
+    "input_str,expected",
+    [
+        (TEST_INPUT, 10),
+        # (REAL_INPUT, 50),
+    ],
+)
+def test_part2(input_str: str, expected: int) -> None:
+    assert calculate_part2(input_str) == expected
