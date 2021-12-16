5eac2f11b28b99d006e14ae8435872dc21061690
Matt Kramer <mkramer@anaconda.com>
2021-12-16 13:50:43 -0600

    Solve day 16

---
+++ y2021/day16/solution.py
@@ -0,0 +1,102 @@
+import math
+from dataclasses import dataclass
+from dataclasses import field
+from pathlib import Path
+from typing import List
+from typing import Optional
+
+INPUTS_FILE = Path(__file__).parent / "input.txt"
+
+
+@dataclass
+class Packet:
+    version: int
+    type_id: int
+    literal_value: Optional[int] = None
+    subpackets: List["Packet"] = field(default_factory=list)
+
+    def version_sum(self) -> int:
+        return self.version + sum(p.version_sum() for p in self.subpackets)
+
+    def evaluate(self) -> int:
+        if self.type_id == 4:
+            assert self.literal_value is not None
+            return self.literal_value
+
+        values = tuple(packet.evaluate() for packet in self.subpackets)
+        if self.type_id == 0:
+            return sum(values)
+        if self.type_id == 1:
+            return math.prod(values)
+        if self.type_id == 2:
+            return min(values)
+        if self.type_id == 3:
+            return max(values)
+        if self.type_id == 5:
+            return int(values[0] > values[1])
+        if self.type_id == 6:
+            return int(values[0] < values[1])
+        if self.type_id == 7:
+            return int(values[0] == values[1])
+        raise ValueError("Unreachable")
+
+
+class BitStream:
+    def __init__(self, hex_string: str):
+        self.bit_string = "".join(
+            f"{int(char, 16):04b}" for char in hex_string.lower().strip()
+        )
+        self.pos = 0
+
+    def read(self, num_bits: int) -> int:
+        substring = self.bit_string[self.pos : self.pos + num_bits]
+        value = int(substring, 2)
+        self.pos += num_bits
+        return value
+
+    def read_packet(self) -> Packet:
+        packet = Packet(version=self.read(3), type_id=self.read(3))
+        if packet.type_id == 4:  # Literal value
+            packet.literal_value = 0
+            while True:
+                continuation = self.read(1)
+                packet.literal_value = (packet.literal_value << 4) | self.read(4)
+                if not continuation:
+                    break
+        else:  # Operator packet
+            length_type_id = self.read(1)
+            if length_type_id == 0:
+                num_bits_in_packet = self.read(15)
+                start_pos = self.pos
+                while self.pos - start_pos < num_bits_in_packet:
+                    subpacket = self.read_packet()
+                    packet.subpackets.append(subpacket)
+            elif length_type_id == 1:
+                num_subpackets = self.read(11)
+                for _ in range(num_subpackets):
+                    subpacket = self.read_packet()
+                    packet.subpackets.append(subpacket)
+        return packet
+
+
+def calculate_part1(input_str: str) -> int:
+    stream = BitStream(input_str)
+    packet = stream.read_packet()
+    return packet.version_sum()
+
+
+def calculate_part2(input_str: str) -> int:
+    stream = BitStream(input_str)
+    packet = stream.read_packet()
+    return packet.evaluate()
+
+
+def main() -> None:
+    with INPUTS_FILE.open() as fp:
+        input_str = fp.read()
+        print(f"The answer to part 1 is {calculate_part1(input_str)}")
+        print(f"The answer to part 2 is {calculate_part2(input_str)}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ y2021/day16/tests.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+
+import pytest
+
+from .solution import calculate_part1
+from .solution import calculate_part2
+
+TEST_INPUT = """
+    some_input
+    abc
+"""
+
+
+INPUTS_FILE = Path(__file__).parent / "input.txt"
+with INPUTS_FILE.open("r") as fp:
+    REAL_INPUT = fp.read()
+
+
+@pytest.mark.parametrize(
+    "input_str,expected",
+    [
+        ("D2FE28", 6),
+        ("38006F45291200", 9),
+        ("EE00D40C823060", 14),
+        ("8A004A801A8002F478", 16),
+        ("620080001611562C8802118E34", 12),
+        ("C0015000016115A2E0802F182340", 23),
+        ("A0016C880162017C3686B18A3D4780", 31),
+        (REAL_INPUT, 986),
+    ],
+)
+def test_part1(input_str: str, expected: int) -> None:
+    assert calculate_part1(input_str) == expected
+
+
+@pytest.mark.parametrize(
+    "input_str,expected",
+    [
+        ("C200B40A82", 3),
+        ("04005AC33890", 54),
+        ("880086C3E88112", 7),
+        ("CE00C43D881120", 9),
+        ("D8005AC2A8F0", 1),
+        ("F600BC2D8F", 0),
+        ("9C005AC2F8F0", 0),
+        ("9C0141080250320F1802104A08", 1),
+        (REAL_INPUT, 18234816469452),
+    ],
+)
+def test_part2(input_str: str, expected: int) -> None:
+    assert calculate_part2(input_str) == expected
