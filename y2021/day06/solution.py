492b1a39b62006ff5ebfb7d42cffd201e6c974f1
Matt Kramer <mkramer@anaconda.com>
2021-12-17 12:38:28 -0600

    Use a Counter

--- y2021/day06/solution.py
+++ y2021/day06/solution.py
@@ -1,34 +1,27 @@
-from collections import defaultdict
+from collections import Counter
 from pathlib import Path
-from typing import Dict
-from typing import List
-
+from typing import Generator
 
 INPUTS_FILE = Path(__file__).parent / "input.txt"
 
 
-def parse(input_str: str) -> List[int]:
-    return [int(s) for s in input_str.split(",")]
+def parse(input_str: str) -> Generator[int, None, None]:
+    for s in input_str.split(","):
+        yield int(s)
 
 
-def advance(data: Dict[int, int]) -> Dict[int, int]:
-    new_data: Dict[int, int] = defaultdict(lambda: 0)
+def advance(data: Counter) -> Counter:
+    new_data: Counter = Counter()
     for age, count in data.items():
         if age == 0:
-            new_data[6] += count
-            new_data[8] += count
+            new_data.update({6: count, 8: count})
         else:
-            new_data[age - 1] += count
+            new_data.update({age - 1: count})
     return new_data
 
 
 def calculate(input_str: str, generations: int) -> int:
-    data = parse(input_str)  # noqa: F841
-
-    fish_counts: Dict[int, int] = defaultdict(lambda: 0)
-    for fish in data:
-        fish_counts[fish] += 1
-
+    fish_counts = Counter(parse(input_str))
     for i in range(generations):
         fish_counts = advance(fish_counts)
     return sum(fish_counts.values())
