0bb802ae47dd51ed19f240023af3d907e249739d
mattkram <mkramer@anaconda.com>
2022-12-03 13:40:18 -0600

    Solve day 2 part 1

--- 2022/src/bin/day02.rs
+++ 2022/src/bin/day02.rs
@@ -20,12 +20,78 @@ fn read_file_to_strings(filename: &str) -> Vec<String> {
     result
 }
 
+
+#[derive(Eq, PartialEq)]
+enum Move {
+    ROCK,
+    PAPER,
+    SCISSORS,
+}
+
+#[derive(Eq, PartialEq)]
+struct Round {
+    you: Move,
+    them: Move,
+}
+
+/// Convert the input list to a list of rounds
+fn lines_to_rounds(lines: Vec<String>) -> Vec<Round> {
+    let mut result: Vec<Round> = Vec::with_capacity(lines.len());
+    for line in lines {
+        let mut split = line.split(" ");
+        let their_move = match split.next().unwrap() {
+            "A" => Move::ROCK,
+            "B" => Move::PAPER,
+            "C" => Move::SCISSORS,
+            _ => panic!("Unknown move")
+        };
+
+        let your_move = match split.next().unwrap() {
+            "X" => Move::ROCK,
+            "Y" => Move::PAPER,
+            "Z" => Move::SCISSORS,
+            _ => panic!("Unknown move"),
+        };
+
+        let game = Round {them:their_move, you: your_move};
+        result.push(game);
+    }
+    result
+}
+
+fn rounds_to_scores(rounds: Vec<Round>) -> Vec<i32> {
+    //The score for a single round is the score for the shape you selected
+    // (1 for Rock, 2 for Paper, and 3 for Scissors) plus the score for the
+    // outcome of the round (0 if you lost, 3 if the round was a draw, and
+    // 6 if you won).
+    let mut result: Vec<i32> = Vec::with_capacity(rounds.len());
+
+    for round in rounds {
+        let mut score = match round.you {
+            Move::ROCK => 1,
+            Move::PAPER => 2,
+            Move::SCISSORS => 3,
+        };
+
+        if round.them == round.you {
+            score += 3;
+        } else if
+            round == (Round{you: Move::ROCK, them: Move::SCISSORS}) ||
+            round == (Round{you: Move::PAPER, them: Move::ROCK}) ||
+            round == (Round{you: Move::SCISSORS, them: Move::PAPER}){
+            score += 6;
+        }
+        result.push(score)
+    }
+    result
+}
+
 fn solve_part1() -> i32 {
     let lines = read_file_to_strings("data/day02.txt");
-    for line in lines {
-        println!("\"{}\"", line);
-    }
-    0
+    let rounds = lines_to_rounds(lines);
+    let scores = rounds_to_scores(rounds);
+
+    scores.iter().sum()
 }
 
 fn solve_part2() -> i32 {
