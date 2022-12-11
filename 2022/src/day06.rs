4bc88419c47c927b1aabd94f320dc7967edfed0c
mattkram <mkramer@anaconda.com>
2022-12-11 09:45:18 -0600

    Generalize and solve day 6 part 2

--- 2022/src/day06.rs
+++ 2022/src/day06.rs
@@ -2,13 +2,13 @@ use std::collections::HashSet;
 use aoc2022;
 
 
-fn find_packet(signal: &String) -> i32 {
+fn find_packet(signal: &String, length: usize) -> i32 {
     for (i, _) in signal.chars().enumerate() {
         // We do a look-forward because it's simpler than ensuring we consider at least 4 chars.
-        let next_four = &signal[i..i+4];
-        let unique: HashSet<char> = HashSet::from_iter(next_four.chars());
-        if unique.iter().count() == 4 {
-            return (i + 4).try_into().unwrap();
+        let packet_candidate = &signal[i..i+length];
+        let unique: HashSet<char> = HashSet::from_iter(packet_candidate.chars());
+        if unique.iter().count() == length {
+            return (i + length).try_into().unwrap();
         }
     }
     panic!("Never found the packet")
@@ -17,12 +17,15 @@ fn find_packet(signal: &String) -> i32 {
 fn solve_part1() -> i32 {
     let lines = aoc2022::read_file_to_strings("data/day06.txt");
     let signal = lines.first().unwrap();
-    let start = find_packet(&signal);
+    let start = find_packet(&signal, 4);
     start
 }
 
 fn solve_part2() -> i32 {
-    0
+    let lines = aoc2022::read_file_to_strings("data/day06.txt");
+    let signal = lines.first().unwrap();
+    let start = find_packet(&signal, 14);
+    start
 }
 
 fn main() {
