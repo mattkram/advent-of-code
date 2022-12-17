0b5b8f9ee09fddc6829334fa5dc84fcf1b278e8e
mattkram <mkramer@anaconda.com>
2022-12-17 00:24:42 -0600

    Solve day 10 part 2

--- 2022/src/day10.py
+++ 2022/src/day10.py
@@ -15,42 +15,60 @@ DIRECTION_MAP = {
 def simulate():
     path = Path("data", "day10.txt")
     register = 1
-    history = [register]
+    history = []
     cycle = 1
     cumulative_signal_strength = 0
     with path.open() as fp:
         for line in fp:
             match line.split():
                 case ["noop"]:
-                    history.append(register)
+                    column = (cycle - 1) % 40
+                    if register - 1 <= column <= register + 1:
+                        history.append("#")
+                    else:
+                        history.append(".")
                     cycle += 1
                     if (cycle - 20) % 40 == 0:
                         cumulative_signal_strength += register * cycle
                 case ["addx", s]:
                     value = int(s)
 
-                    history.append(register)
+                    column = (cycle - 1) % 40
+                    if register - 1 <= column <= register + 1:
+                        history.append("#")
+                    else:
+                        history.append(".")
                     cycle += 1
                     if (cycle - 20) % 40 == 0:
                         cumulative_signal_strength += register * cycle
 
-                    history.append(register)
+                    column = (cycle - 1) % 40
+                    if register - 1 <= column <= register + 1:
+                        history.append("#")
+                    else:
+                        history.append(".")
                     register += value
                     cycle += 1
                     if (cycle - 20) % 40 == 0:
                         cumulative_signal_strength += register * cycle
 
-    return cumulative_signal_strength
+    return cumulative_signal_strength, history
 
 
 def solve_part1() -> int:
-    return simulate()
+    return simulate()[0]
 
 
-def solve_part2() -> int:
-    return simulate()
+def solve_part2() -> None:
+    history = simulate()[1]
+    for row in range(len(history) // 40):
+        line = "".join(history[40 * row : 40 * (row + 1)])
+        line = line.replace(".", "\N{MEDIUM BLACK CIRCLE}").replace(
+            "#", "\N{LARGE YELLOW CIRCLE}"
+        )
+        print(line)
 
 
 if __name__ == "__main__":
     print(f"The answer to part 1 is: {solve_part1()}")
-    print(f"The answer to part 2 is: {solve_part2()}")
+    solve_part2()
