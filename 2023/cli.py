61a5bdcca03038cc6a7dc6b9caf8dd74bb40e6a0
mattkram <mkramer@anaconda.com>
2023-12-15 22:23:26 -0600

    Add stupid CLI

---
+++ 2023/cli.py
@@ -0,0 +1,46 @@
+import os
+from pathlib import Path
+
+import requests
+import typer
+from dotenv import load_dotenv
+from rich.console import Console
+
+load_dotenv()
+
+SESSION_COOKIE = os.environ.get("SESSION_COOKIE")
+
+
+app = typer.Typer(add_completion=False, help="Welcome to the Anaconda CLI!")
+console = Console()
+
+
+@app.callback(invoke_without_command=True, no_args_is_help=True)
+def main() -> None:
+    """Anaconda CLI."""
+
+
+@app.command()
+def get() -> None:
+    """Get data for a certain day."""
+    year = 2023
+    day = 6
+    input_file = Path(f"day{day:02}", "input.txt")
+
+    if input_file.exists():
+        console.print(f"Data file [cyan]{input_file}[/cyan] exists, skipping request")
+        raise typer.Abort()
+
+    response = requests.get(
+        f"https://adventofcode.com/{year}/day/{day}/input",
+        cookies={"session": SESSION_COOKIE},
+    )
+    assert response.status_code == 200
+    console.print(response.text)
+    content = response.text
+    with input_file.open("w") as fp:
+        fp.write(content)
+
+
+if __name__ == "__main__":
+    app()
