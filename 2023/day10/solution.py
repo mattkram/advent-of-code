4d9357112e3e3ad562a1c0d555e495de71d9b1d1
mattkram <mkramer@anaconda.com>
2023-12-19 23:37:13 -0600

    Solve day 10 part 2

--- 2023/day10/solution.py
+++ 2023/day10/solution.py
@@ -63,11 +63,8 @@ def trace_path(joints, start, first_step):
     return path
 
 
-def calculate_part1(input_str: str) -> int:
-    start, joints = parse(input_str)  # noqa: F841
-
+def find_path(joints, start):
     # Loop through the four adjacent neighbors to see if there is a joint
-    path = []
     for step in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
         try:
             path = trace_path(joints, start, step)
@@ -75,15 +72,45 @@ def calculate_part1(input_str: str) -> int:
             # Either we started where there was no joint, or we hit a dead end
             continue
         else:
-            break
+            return path
 
+
+def calculate_part1(input_str: str) -> int:
+    start, joints = parse(input_str)  # noqa: F841
+
+    path = find_path(joints, start)
     path_length = len(path)
     return path_length // 2 + path_length % 2
 
 
+def count_interior_tiles(joints, path):
+    loop = set(path)  # Used to more efficiently check membership
+    enclosed = set()
+
+    for curr, next in zip(path, path[1:]):
+        (dx, dy) = (next[0] - curr[0], next[1] - curr[1])
+
+        # 90 deg, counter-clockwise
+        search_dir = dy, -dx
+
+        # Search in that direction until we hit a part of the loop
+        # We need to search from both the current and next location
+        # This covers corners, where the direction will change and
+        # next -> curr.
+        for xy in [curr, next]:
+            while (xy := (xy[0] + search_dir[0], xy[1] + search_dir[1])) not in loop:
+                enclosed.add(xy)
+                if max(max(xy), abs(min(xy))) > 1000:
+                    # If we go off the board, we need to go the other way through the loop
+                    return count_interior_tiles(joints, path[::-1])
+
+    return len(enclosed)
+
+
 def calculate_part2(input_str: str) -> int:
-    data = parse(input_str)  # noqa: F841
-    raise ValueError("Cannot find an answer")
+    start, joints = parse(input_str)  # noqa: F841
+    path = find_path(joints, start)
+    return count_interior_tiles(joints, path)
 
 
 def main() -> None:
--- 2023/day10/tests.py
+++ 2023/day10/tests.py
@@ -24,6 +24,47 @@ TEST_INPUT_2 = textwrap.dedent(
     LJ...
     """
 )
+TEST_INPUT_3 = textwrap.dedent(
+    """\
+    ...........
+    .S-------7.
+    .|F-----7|.
+    .||.....||.
+    .||.....||.
+    .|L-7.F-J|.
+    .|..|.|..|.
+    .L--J.L--J.
+    ...........
+    """
+)
+TEST_INPUT_4 = textwrap.dedent(
+    """\
+    .F----7F7F7F7F-7....
+    .|F--7||||||||FJ....
+    .||.FJ||||||||L7....
+    FJL7L7LJLJ||LJ.L-7..
+    L--J.L7...LJS7F-7L7.
+    ....F-J..F7FJ|L7L7L7
+    ....L7.F7||L7|.L7L7|
+    .....|FJLJ|FJ|F7|.LJ
+    ....FJL-7.||.||||...
+    ....L---J.LJ.LJLJ...
+    """
+)
+TEST_INPUT_5 = textwrap.dedent(
+    """\
+    FF7FSF7F7F7F7F7F---7
+    L|LJ||||||||||||F--J
+    FL-7LJLJ||||||LJL-77
+    F--JF--7||LJLJ7F7FJ-
+    L---JF-JLJ.||-FJLJJ7
+    |F|F-JF---7F7-L7L|7|
+    |FFJF7L7F-JF7|JL---7
+    7-L-JL7||F7|L7F-7F7|
+    L.L7LFJ|||||FJL7||LJ
+    L7JLJL-JLJLJL--JLJ.L
+    """
+)
 
 INPUTS_FILE = Path(__file__).parent / "input.txt"
 with INPUTS_FILE.open("r") as fp:
@@ -33,8 +74,8 @@ with INPUTS_FILE.open("r") as fp:
 @pytest.mark.parametrize(
     "input_str,expected",
     [
-        pytest.param(TEST_INPUT_1, 4, id="test-input"),
-        pytest.param(TEST_INPUT_2, 8, id="test-input"),
+        pytest.param(TEST_INPUT_1, 4, id="test-input-1"),
+        pytest.param(TEST_INPUT_2, 8, id="test-input-2"),
         pytest.param(REAL_INPUT, 6942, id="real-data"),
     ],
 )
@@ -45,8 +86,10 @@ def test_part1(input_str: str, expected: int) -> None:
 @pytest.mark.parametrize(
     "input_str,expected",
     [
-        pytest.param(TEST_INPUT_1, 114, id="test-input"),
-        # pytest.param(REAL_INPUT, 1921197370, id="real-data"),
+        pytest.param(TEST_INPUT_3, 4, id="test-input-3"),
+        pytest.param(TEST_INPUT_4, 8, id="test-input-4"),
+        pytest.param(TEST_INPUT_5, 10, id="test-input-5"),
+        pytest.param(REAL_INPUT, 297, id="real-data"),
     ],
 )
 def test_part2(input_str: str, expected: int) -> None:
