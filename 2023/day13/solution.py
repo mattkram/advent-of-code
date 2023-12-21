b2568530569849e89eb23e3b36c510c373672065
mattkram <mkramer@anaconda.com>
2023-12-21 15:31:05 -0600

    Solve day 13 part 1

---
+++ 2023/day13/solution.py
@@ -0,0 +1,63 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+INPUTS_FILE = Path(__file__).parent / "input.txt"
+
+
+def parse(input_str: str) -> list[list[str]]:
+    patterns = []
+    for section in input_str.strip().split("\n\n"):
+        lines = [s.strip() for s in section.splitlines() if s.strip()]
+        patterns.append(lines)
+    return patterns
+
+
+def find_reflection(pattern: list[str]) -> int | None:
+    """Find the reflection point horizontally, if it exists, otherwise return None."""
+
+    reflect_indexes = [i for i, p in enumerate(pattern[:-1]) if pattern[i + 1] == p]
+
+    for reflect_index in reflect_indexes:
+        left = pattern[reflect_index::-1]
+        right = pattern[reflect_index + 1 :]
+        if all(a == b for a, b in zip(left, right)):
+            return reflect_index + 1
+    return None
+
+
+def transpose(pattern: list[str]) -> list[str]:
+    result = []
+    for col in zip(*pattern):
+        result.append("".join(col))
+
+    return result
+
+
+def calculate_part1(input_str: str) -> int:
+    patterns = parse(input_str)
+
+    result = 0
+    for pattern in patterns:
+        if (score := find_reflection(pattern)) is not None:
+            result += 100 * score
+        elif (score := find_reflection(transpose(pattern))) is not None:
+            result += score
+
+    return result
+
+
+def calculate_part2(input_str: str) -> int:
+    data = parse(input_str)  # noqa: F841
+    raise ValueError("Cannot find an answer")
+
+
+def main() -> None:
+    with INPUTS_FILE.open() as fp:
+        input_str = fp.read()
+        print(f"The answer to part 1 is {calculate_part1(input_str)}")
+        print(f"The answer to part 2 is {calculate_part2(input_str)}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2023/day13/tests.py
@@ -0,0 +1,54 @@
+import textwrap
+from pathlib import Path
+
+import pytest
+
+from .solution import calculate_part1
+from .solution import calculate_part2
+
+TEST_INPUT = textwrap.dedent(
+    """\
+    #.##..##.
+    ..#.##.#.
+    ##......#
+    ##......#
+    ..#.##.#.
+    ..##..##.
+    #.#.##.#.
+
+    #...##..#
+    #....#..#
+    ..##..###
+    #####.##.
+    #####.##.
+    ..##..###
+    #....#..#
+    """
+)
+
+
+INPUTS_FILE = Path(__file__).parent / "input.txt"
+with INPUTS_FILE.open("r") as fp:
+    REAL_INPUT = fp.read()
+
+
+@pytest.mark.parametrize(
+    "input_str,expected",
+    [
+        pytest.param(TEST_INPUT, 405, id="test-input"),
+        pytest.param(REAL_INPUT, 30158, id="real-data"),
+    ],
+)
+def test_part1(input_str: str, expected: int) -> None:
+    assert calculate_part1(input_str) == expected
+
+
+@pytest.mark.parametrize(
+    "input_str,expected",
+    [
+        pytest.param(TEST_INPUT, 114, id="test-input"),
+        # pytest.param(REAL_INPUT, 1921197370, id="real-data"),
+    ],
+)
+def test_part2(input_str: str, expected: int) -> None:
+    assert calculate_part2(input_str) == expected
