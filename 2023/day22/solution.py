4508947216e2ccc95674f22e36e0636bb4266e12
mattkram <mkramer@anaconda.com>
2023-12-22 10:46:48 -0600

    Pause

--- 2023/day22/solution.py
+++ 2023/day22/solution.py
@@ -17,7 +17,9 @@ def parse(input_str: str) -> List[int]:
         x_min, x_max = min(left[0], right[0]), max(left[0], right[0])
         y_min, y_max = min(left[1], right[1]), max(left[1], right[1])
         z_min, z_max = min(left[2], right[2]), max(left[2], right[2])
-
+        dims = (x_max + 1 - x_min, y_max + 1 - y_min, z_max + 1 - z_min)
+        if 1 not in dims:
+            raise ValueError(f"Brick is not one-dimensional: {dims}")
         for x in range(x_min, x_max + 1):
             for y in range(y_min, y_max + 1):
                 for z in range(z_min, z_max + 1):
@@ -27,7 +29,7 @@ def parse(input_str: str) -> List[int]:
     return bricks
 
 
-def move_down(bricks):
+def move_down(bricks, exit_early=False):
     """Continuously move bricks downward one step at a time until they settle."""
     did_move = False
     while True:
@@ -49,6 +51,8 @@ def move_down(bricks):
                 # Move the brick down
                 has_moved = True
                 did_move = True
+                if exit_early:
+                    return True
                 bricks[i] = list(one_below)
 
         if not has_moved:
@@ -58,14 +62,14 @@ def move_down(bricks):
 
 def calculate_part1(input_str: str) -> int:
     bricks = parse(input_str)
-    move_down(bricks)
+    # move_down(bricks)
 
     result = 0
     for i in range(len(bricks)):
         # First make a copy but ignore one of the bricks
         bricks_copy = [list(brick) for j, brick in enumerate(bricks) if i != j]
 
-        did_move = move_down(bricks_copy)
+        did_move = move_down(bricks_copy, exit_early=True)
         if not did_move:
             result += 1
     return result
