2f9fb2a44a96ab8a4b3469f93918f5d3c9d70f33
mattkram <mkramer@anaconda.com>
2023-12-22 22:22:39 -0600

    Solve day 15 part 1

---
+++ 2023/day15/solution.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+
+INPUTS_FILE = Path(__file__).parent / "input.txt"
+
+
+def parse(input_str: str) -> list[str]:
+    return input_str.strip().split(",")
+
+
+def HASH(string: str) -> int:
+    value = 0
+    for char in string:
+        value += ord(char)
+        value *= 17
+        value %= 256
+
+    return value
+
+
+def calculate_part1(input_str: str) -> int:
+    sequence = parse(input_str)
+    return sum(HASH(step) for step in sequence)
+
+
+def calculate_part2(input_str: str) -> int:
+    data = parse(input_str)  # noqa: F841
+    raise ValueError("Cannot find an answer")
+
+
+def main() -> None:
+    with INPUTS_FILE.open() as fp:
+        input_str = fp.read()
+        print(f"The answer to part 1 is {calculate_part1(input_str)}")
+        print(f"The answer to part 2 is {calculate_part2(input_str)}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2023/day15/tests.py
@@ -0,0 +1,40 @@
+import textwrap
+from pathlib import Path
+
+import pytest
+
+from .solution import calculate_part1
+from .solution import calculate_part2
+
+TEST_INPUT = textwrap.dedent(
+    """\
+    rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7
+    """
+)
+
+
+INPUTS_FILE = Path(__file__).parent / "input.txt"
+with INPUTS_FILE.open("r") as fp:
+    REAL_INPUT = fp.read()
+
+
+@pytest.mark.parametrize(
+    "input_str,expected",
+    [
+        pytest.param(TEST_INPUT, 1320, id="test-input"),
+        pytest.param(REAL_INPUT, 507769, id="real-data"),
+    ],
+)
+def test_part1(input_str: str, expected: int) -> None:
+    assert calculate_part1(input_str) == expected
+
+
+@pytest.mark.parametrize(
+    "input_str,expected",
+    [
+        pytest.param(TEST_INPUT, 114, id="test-input"),
+        # pytest.param(REAL_INPUT, 1921197370, id="real-data"),
+    ],
+)
+def test_part2(input_str: str, expected: int) -> None:
+    assert calculate_part2(input_str) == expected
